638674b259c3c16f6a6226ea3675a512682203cf
Jose Alvarez <jealv@icloud.com>
2024-03-09 18:45:10 -0500

    chore: fix typo in comment

--- src/utils/make-osm-link.ts
+++ src/utils/make-osm-link.ts
@@ -2,7 +2,7 @@ import { GeocodingResult } from "../types";
 
 export const makeOsmLink = ({ id, provider, lat, lng }: GeocodingResult) => {
 	if (provider === "free-geocoding-api") {
-		// theoreticall we can use the osm_id from free-geocoding-api here,
+		// theoretically we can use the osm_id from free-geocoding-api here,
 		// but I can't find any relevant documentation
 	}
 	const params = new URLSearchParams({
